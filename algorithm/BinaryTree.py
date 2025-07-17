05a4932e9810fafb048eee706bcb7e5f9325f48e
823849713@qq.com <823849713@qq.com>
2025-07-18 07:30:55 +0800

    二叉树遍历

--- algorithm/BinaryTree.py
+++ algorithm/BinaryTree.py
@@ -1,5 +1,7 @@
 """二叉树遍历"""
 
+from collections import deque
+
 
 class BinaryTree:
 
@@ -10,6 +12,8 @@ class BinaryTree:
         b = BinaryTree.TreeNode("b", d, e)
         c = BinaryTree.TreeNode("c", f, None)
         self.root = BinaryTree.TreeNode("a", b, c)
+
+        self.stack = deque()
         pass
 
     def prev(self, tree_node):
@@ -24,6 +28,22 @@ class BinaryTree:
         self.prev(tree_node.left)
         self.prev(tree_node.right)
 
+        pass
+
+    def prev_while(self, tree_node):
+        node = tree_node
+        curr_node = node
+        while node is not None or len(self.stack) != 0:
+            if node is not None:
+                print(f"{node.value} ", end="")
+                self.stack.append(node)
+                curr_node = node
+                node = node.left
+            else:
+                pop = self.stack.pop()
+                if curr_node != node:
+                    node = pop.right
+            pass
         pass
 
     def middle(self, tree_node):
@@ -57,6 +77,11 @@ class BinaryTree:
             self.right = right
             pass
 
+        def __eq__(self, other):
+            if not isinstance(other, BinaryTree.TreeNode):
+                return False
+            return self.value == other.value  # 按属性值比较
+
         pass
 
     pass
@@ -65,5 +90,6 @@ class BinaryTree:
 if __name__ == "__main__":
     tree = BinaryTree()
     tree.prev(tree.root)
+    tree.prev_while(tree.root)
 
     pass
