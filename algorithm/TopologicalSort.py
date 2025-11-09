fc682d2b3fa3f1b5cb22dfb3362f10a7afd85546
823849713@qq.com <823849713@qq.com>
2025-11-09 08:26:04 +0800

    拓扑排序

---
+++ algorithm/TopologicalSort.py
@@ -0,0 +1,79 @@
+"""
+拓扑排序
+"""
+from algorithm.Edge import Edge
+from algorithm.Vertex import Vertex
+
+
+class TopologicalSort:
+
+    def topological_sort(self, graph):
+        for vertex in graph:
+            for edge in vertex.edges:
+                edge.linked.in_degree += 1
+
+        vertex_queue = []
+        for vertex in graph:
+            if vertex.in_degree == 0:
+                vertex_queue.append(vertex)
+
+        while len(vertex_queue) > 0:
+            vertex = vertex_queue.pop(0)
+            print(f"{vertex.name}")
+            for edge in vertex.edges:
+                edge.linked.in_degree -= 1
+                if edge.linked.in_degree == 0:
+                    vertex_queue.append(edge.linked)
+        pass
+
+    def topological_sort2(self, graph):
+        stack = []
+        for vertex in graph:
+            self.__dfs(vertex, stack)
+
+        for result in stack[::-1]:
+            print(f"{result.name}")
+
+        pass
+
+    def __dfs(self, vertex, stack):
+
+        if vertex.visited:
+            return
+
+        for edge in vertex.edges:
+            self.__dfs(edge.linked, stack)
+
+        vertex.visited = True
+        stack.append(vertex)
+
+        pass
+
+    pass
+
+
+if __name__ == "__main__":
+    v1 = Vertex("网页基础")
+    v2 = Vertex("java基础")
+    v3 = Vertex("javaWeb")
+    v4 = Vertex("spring框架")
+    v5 = Vertex("微服务框架")
+    v6 = Vertex("数据库")
+    v7 = Vertex("实战项目")
+
+    v1.edges = [Edge(v3)]
+    v2.edges = [Edge(v3)]
+    v3.edges = [Edge(v4)]
+    v6.edges = [Edge(v4)]
+    v4.edges = [Edge(v5)]
+    v5.edges = [Edge(v7)]
+    v7.edges = []
+
+    graph = [v1, v2, v3, v4, v5, v6, v7]
+
+    topological_sort = TopologicalSort()
+    # topological_sort.topological_sort(graph)
+
+    topological_sort.topological_sort2(graph)
+
+    pass
--- algorithm/Vertex.py
+++ algorithm/Vertex.py
@@ -8,6 +8,7 @@ class Vertex:
         self.name = name
         self.edges = list()
         self.visited = False
+        self.in_degree = 0
 
 
 if __name__ == "__main__":
