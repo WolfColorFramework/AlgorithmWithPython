1a2e1aadf393ef44aee63624102ed9d826ba41e3
823849713@qq.com <823849713@qq.com>
2025-11-16 20:32:36 +0800

    dijkstra

--- algorithm/Dijkstra.py
+++ algorithm/Dijkstra.py
@@ -16,12 +16,12 @@ class Dijkstra:
 
         while len(min_heap) > 0:
             # 选取当前最小顶点
-            _, curr = heapq.heappop(min_heap)   # 弹出最小堆的元素
+            _, curr = heapq.heappop(min_heap)  # 弹出最小堆的元素
 
             # 更新当前顶点的邻居的距离
-            self.__update_neighbor_distance(curr, min_heap)
-
-            curr.visited = True
+            if curr.visited is False:
+                self.__update_neighbor_distance(curr, min_heap)
+                curr.visited = True
 
             pass
 
@@ -36,7 +36,7 @@ class Dijkstra:
                 if edge.linked.distance > vertex.distance + edge.weight:
                     edge.linked.distance = vertex.distance + edge.weight
                     edge.linked.pre = vertex
-                    heapq.heappush(min_heap, (edge.linked.distance, edge.linked))   # 重新插入，使最小堆重新排序
+                    heapq.heappush(min_heap, (edge.linked.distance, edge.linked))  # 重新插入，使最小堆重新排序
                 pass
 
     pass
