e4f20a3fee5be6fbae4583d5a10cdff15f63da5e
823849713@qq.com <823849713@qq.com>
2026-02-14 22:03:42 +0800

    背包问题，可切分

---
+++ algorithm/KnapsackProblem.py
@@ -0,0 +1,52 @@
+"""
+背包问题
+"""
+
+
+class KnapsackProblem:
+    def __init__(self):
+        self.items = [
+            KnapsackProblem.PackageItem(0, 4, 24),
+            KnapsackProblem.PackageItem(1, 8, 160),
+            KnapsackProblem.PackageItem(2, 2, 4000),
+            KnapsackProblem.PackageItem(3, 6, 108),
+            KnapsackProblem.PackageItem(4, 1, 4000),
+        ]
+        pass
+
+    def solve(self, total_weight):
+        self.items.sort(key=lambda x: x.unit_value, reverse=True)
+
+        max = 0
+        for item in self.items:
+            if total_weight >= item.weight:
+                max += item.value
+                total_weight -= item.weight
+            else:
+                max += item.unit_value * total_weight
+                break
+
+        print(max)
+        pass
+
+
+    class PackageItem:
+        def __init__(self, index, weight, value):
+            self.index = index
+            self.weight = weight
+            self.value = value
+            self.unit_value = value / weight
+            pass
+
+        def __repr__(self):
+            return f"{self.index} {self.weight} {self.value}"
+
+        pass
+
+
+if __name__ == "__main__":
+
+    problem = KnapsackProblem()
+    problem.solve(10)
+
+    pass
