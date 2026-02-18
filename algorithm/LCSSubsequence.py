333e6bf7bbe18a60ae80216217e38f7ee8eadaf9
823849713@qq.com <823849713@qq.com>
2026-02-18 16:12:19 +0800

    最长公共子序列

--- algorithm/LCSSubsequence.py
+++ algorithm/LCSSubsequence.py
@@ -1,9 +1,9 @@
 """
-最长公共子串
+最长公共子序列
 """
 
 
-class LCSSubstring:
+class LCSSubsequence:
     def __init__(self):
         pass
 
@@ -11,31 +11,30 @@ class LCSSubstring:
         """
         :param word1: 源字符串
         :param word2: 比较字符串
-        :return: 公共子串长度
+        :return: 最长公共子序列
         """
 
         # 行:word2；列:word1
         dp = [[0 for _ in range(len(word1))] for _ in range(len(word2))]
 
-        max_length = 0
         for row_index, row_char in enumerate(word2):
             for col_index, col_char in enumerate(word1):
-                if row_char == col_char:
+                if row_char == col_char:    # 相同字符
                     if row_index == 0 or col_index == 0:
                         dp[row_index][col_index] = 1
-                        max_length = max(max_length, dp[row_index][col_index])
                     dp[row_index][col_index] = dp[row_index - 1][col_index - 1] + 1
-                    max_length = max(max_length, dp[row_index][col_index])
-                else:
-                    dp[row_index][col_index] = 0
+                else:   # 不同字符
+                    dp[row_index][col_index] = max(dp[row_index - 1][col_index], dp[row_index][col_index - 1])
 
-        return max_length
+        for row in dp:
+            print(row)
+
+        return dp[-1][-1]
 
         pass
 
 
 if __name__ == "__main__":
-    lcs = LCSSubstring()
-    print(lcs.lcs("itheima", "thema"))
-
+    lcs = LCSSubsequence()
+    print(lcs.lcs("abcxyz", "abxyz"))
     pass
