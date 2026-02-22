31ee12dca8bad4b7b8b339bc3f7e68f0f6b3e83c
823849713@qq.com <823849713@qq.com>
2026-02-22 11:07:20 +0800

    最长公共子序列

---
+++ algorithm/Catalan.py
@@ -0,0 +1,30 @@
+"""
+卡特兰树
+"""
+
+
+class Catalan:
+    def __init__(self):
+        pass
+
+    def catalan(self, n):
+        dp = list([0] * (n + 1))
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):  # i:第i个卡特兰树
+            # 拆分的循环
+            for j in range(i):
+                print(f"({j},{i - 1 - j})", end="\t")
+                dp[i] += dp[j] * dp[i - 1 - j]
+            print("", end="\n")
+
+        return dp[n]
+
+        pass
+
+
+if __name__ == "__main__":
+    catalan = Catalan()
+    result = catalan.catalan(5)
+    print(result)
+    pass
