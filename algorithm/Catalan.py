170320d4d2804a8fff4c74005046f51e7540249d
823849713@qq.com <823849713@qq.com>
2026-02-22 11:08:10 +0800

    卡特兰树

--- algorithm/Catalan.py
+++ algorithm/Catalan.py
@@ -12,8 +12,7 @@ class Catalan:
         dp[0] = 1
         dp[1] = 1
         for i in range(2, n + 1):  # i:第i个卡特兰树
-            # 拆分的循环
-            for j in range(i):
+            for j in range(i):  # 拆分的循环
                 print(f"({j},{i - 1 - j})", end="\t")
                 dp[i] += dp[j] * dp[i - 1 - j]
             print("", end="\n")
